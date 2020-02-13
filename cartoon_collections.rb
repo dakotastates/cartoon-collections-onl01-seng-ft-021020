da78b4589ae5330378c130997424f502fbec21e7
Dakota States <dakota.states@gmail.com>
2020-02-13 17:29:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,12 @@ def roll_call_dwarves(names)
 names.each_with_index {|name, index| puts "#{index + 1} #{name}"}
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|call| "#{call.upcase}!" }
-end
+def summon_captain_planet(veggies)
+  
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
+  end
 
 
 def long_planeteer_calls(calls) 
